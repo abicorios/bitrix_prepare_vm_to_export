9a57ceebfb7a76b6c9e775d57d15a13d0ca82816
abicorios <yaroslav.moroz91@gmail.com>
2021-09-29 13:30:40 +0300

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,6 @@
 sed -Ei 's/^(PasswordAuthentication) (no)/\1 yes/' /etc/ssh/sshd_config
 service sshd restart
 echo 'bitrix:github123' | chpasswd
+echo 'root:github123' | chpasswd
 usermod -aG vboxsf bitrix
 
